40866085f12b3f4d026938c8a19aa581d5762b97
dlapochkin <d.lapochkin@g.nsu.ru>
2022-11-24 13:32:58 +0700

    update branch

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE web;
 USE web;
 SET GLOBAL time_zone = '+7:00';
 
-create table recording(
+create table record(
 	id binary(16),
     title varchar(64) not null,
     address varchar(64) not null,
