88687dfee544155247f6a4d9c23de61e93e248fc
ikrut555 <44076851+ikrut555@users.noreply.github.com>
2022-12-01 11:50:48 +0700

    Create UserRepository.java

---
+++ src/main/java/rest/persistence/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package rest.persistence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rest.persistence.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
