85892d8ed714b8eaf721366aef7b68e7b2caebe1
ikrut555 <44076851+ikrut555@users.noreply.github.com>
2022-12-15 20:56:33 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -25,6 +25,21 @@ create table record(
     foreign key (user_id) references user (id)
 );
 
+create table booking(
+	id binary(16),
+    title varchar(64) not null,
+    address varchar(64) not null,
+    berth tinyint not null,
+    rent int not null,
+    description text,
+    image varchar(64),
+    primary key (id),
+    user_id int,
+    foreign key (user_id) references user (id)
+    -- может быть есть смысл сделать через связь booking и record, а не ввиде самостоятельных таблиц
+    -- foreign key (id) references record (id)
+);
+
 create table role(
 	id int not null auto_increment,
     name varchar(64),
@@ -43,3 +58,4 @@ select * from record;
 select * from user;
 select * from role;
 select * from user_roles;
+select * from booking;
